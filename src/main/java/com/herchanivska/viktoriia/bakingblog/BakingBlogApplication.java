98febf08c5b5fc701532ae8f9547d54dd074e480
Viktoriia Herchanivska <viktoriia.herchanivska@gmail.com>
2024-05-03 18:10:37 +0300

    initial commit

---
+++ src/main/java/com/herchanivska/viktoriia/bakingblog/BakingBlogApplication.java
@@ -0,0 +1,13 @@
+package com.herchanivska.viktoriia.bakingblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakingBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakingBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herchanivska/viktoriia/bakingblog/BakingBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herchanivska.viktoriia.bakingblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakingBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
