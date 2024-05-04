24f674e03ccd2fb0097b7402a25acd6da99e2edf
Viktoriia Herchanivska <viktoriia.herchanivska@gmail.com>
2024-05-04 18:32:39 +0300

    update User validateion, add User validation tests

--- src/main/java/com/herchanivska/viktoriia/bakingblog/model/User.java
+++ src/main/java/com/herchanivska/viktoriia/bakingblog/model/User.java
@@ -22,44 +22,44 @@ public class User {
     private long id;
 
     @Column(unique = true)
-    @NotEmpty(message = "Email can not be null or empty")
+    @NotNull(message = "Email can not be null")
     @Pattern(regexp = "^[\\w.-]+@([\\w-]+\\.)+\\w{2,4}$", message = "Please provide valid email")
     private String email;
 
     @Column(unique = true)
-    @NotEmpty(message = "Username can not be null or empty")
-    @Pattern(regexp = "[a-zA-Z0-9_-]+", message = "Username could contain only letters, numbers, dash and underscore")
+    @NotNull(message = "Username can not be null")
+    @Pattern(regexp = "[\\w-]+", message = "Username could contain only letters, numbers, dash and underscore")
     @Size(min = 3, max = 25, message = "Username should be more than or equal to {min} and less than or equal to {max}")
     private String username;
 
-    @NotEmpty(message = "Password can not be null or empty")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])$",
+    @NotNull(message = "Password can not be null")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).*$",
             message = "Password should contain at least one lower case letter, " +
                     "one upper case letter, one digit, one special character")
-    @Size(min = 8, message = "Password should be at least 8 characters in length")
+    @Size(min = 8, message = "Password should be at least {min} characters in length")
     private String password;
 
-    @Past
+    @Past(message = "Birth date should be before today")
     private LocalDate birthDate;
 
     @OneToMany(targetEntity = Recipe.class, mappedBy = "author")
-    List<Recipe> myRecipes;
+    private List<Recipe> myRecipes;
 
     @ManyToMany(targetEntity = Recipe.class)
     @JoinTable(name = "saved_recipes",
     joinColumns = @JoinColumn(name = "user_id"),
     inverseJoinColumns = @JoinColumn(name = "recipe_id", referencedColumnName = "id"))
-    Set<Recipe> savedRecipes;
+    private Set<Recipe> savedRecipes;
 
     @ManyToMany(targetEntity = Recipe.class)
     @JoinTable(name = "liked_recipes",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "recipe_id", referencedColumnName = "id"))
-    List<Recipe> likedRecipes;
+    private List<Recipe> likedRecipes;
 
     @ManyToMany(targetEntity = User.class)
     @JoinTable(name = "user_followers",
     joinColumns = @JoinColumn(name = "user_id"),
     inverseJoinColumns = @JoinColumn(name = "follower_id", referencedColumnName = "id"))
-    List<User> followers;
+    private List<User> followers;
 }
